e50320431dbd2951344c26e806e88d432e58d196
0kaa <thebestoka0@gmail.com>
2022-10-11 22:40:36 +0200

    🐛 FIX: FirstArticle

--- components/FirstArticle.vue
+++ components/FirstArticle.vue
@@ -13,7 +13,7 @@
         </div>
 
         <div
-          class="before:top top-0 right-0 h-full rounded-tl-[4px] border-t-[3px] border-l-[3px] border-black bg-cover pt-5 before:absolute before:left-2.5 before:top-2.5 before:block before:h-[calc(100%_-_10px)] before:w-[calc(100%_-_10px)] before:rounded-tl-[4px] before:border-t-[3px] before:border-l-[3px] before:border-black md:absolute md:w-full md:overflow-hidden md:rounded-[4px] md:pt-0 before:md:hidden"
+          class="before:top top-0 right-0 h-full rounded-tl-[4px] border-t-[3px] border-l-[3px] border-black bg-cover pt-5 before:pointer-events-none before:absolute before:left-2.5 before:top-2.5 before:block before:h-[calc(100%_-_10px)] before:w-[calc(100%_-_10px)] before:rounded-tl-[4px] before:border-t-[3px] before:border-l-[3px] before:border-black md:absolute md:w-full md:overflow-hidden md:rounded-[4px] md:pt-0 before:md:hidden"
         >
           <div
             class="article-image top-0 right-0 flex h-full flex-col items-center justify-center gap-5 rounded-[4px] bg-cover px-6 md:absolute md:flex-row md:px-10 md:text-left lg:justify-between"
@@ -21,8 +21,20 @@
           >
             <h3
               v-text="article.title"
-              class="w-full text-2xl font-bold text-black underline md:w-2/3 md:text-4xl md:text-white md:no-underline"
+              class="hidden w-full text-2xl font-bold text-black underline md:block md:w-2/3 md:text-4xl md:text-white md:no-underline"
             />
+            <nuxt-link
+              :to="{
+                name: 'category-article',
+                params: {
+                  category: article.post_categories[0].slug,
+                  article: article.slug,
+                },
+              }"
+              class="w-full text-2xl font-bold text-black underline md:hidden md:w-2/3 md:text-4xl md:text-white md:no-underline"
+              v-text="article.title"
+            />
+
             <nuxt-link
               :to="{
                 name: 'category-article',
