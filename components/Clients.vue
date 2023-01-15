7a8848e6f9f1ffc07a35fb431a5e980c045b5758
0kaa <thebestoka0@gmail.com>
2023-01-15 12:54:02 +0200

    👌 IMPROVE: Clients slick

--- components/Clients.vue
+++ components/Clients.vue
@@ -3,7 +3,7 @@
     <div
       class="relative px-4 py-4 overflow-hidden text-white bg-black client-slick lg:px-10 lg:py-10"
     >
-      <slick ref="client-slick" :options="slickOptions" class="flex">
+      <slick ref="client_slick" :options="slickOptions" class="flex">
         <nuxt-link
           :to="{
             name: 'csapatok-series-team',
@@ -43,6 +43,12 @@ export default {
     teams() {
       return this.$store.getters.getTeams
     },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$refs.client_slick.reSlick()
+      this.$refs.client_slick.goTo(0, true)
+    })
   },
   data: () => ({
     slickOptions: {
