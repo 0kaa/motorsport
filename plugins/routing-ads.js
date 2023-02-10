911fe5627050256fc2256adfcf8ec4e2c7a0414c
0kaa <thebestoka0@gmail.com>
2023-02-10 15:58:08 +0200

    📦 NEW: prevent routing

--- plugins/routing-ads.js
+++ plugins/routing-ads.js
@@ -1,5 +1,8 @@
 export default ({ app }) => {
     app.router.afterEach((to, from) => {
-        window.__adsConfig.repopulate()
+        // window.__adsConfig.repopulate()
+        // if (typeof window !== 'undefined' && window.__adsConfig && to.path !== from.path) {
+        //     window.__adsConfig.repopulate()
+        // }
     })
 }
