be024d1922fd9be92de4f6c9780deaf990c817bc
0kaa <thebestoka0@gmail.com>
2023-03-06 08:29:45 +0200

    🐛 FIX: connection articles link

--- pages/_category/_article/index.vue
+++ pages/_category/_article/index.vue
@@ -10,9 +10,7 @@
         </div>
         <div class="flex flex-col gap-6">
           <nuxt-link
-            :to="`/${
-              !category ? article.post_categories[0].slug : category.slug
-            }/${article.slug}`"
+            :to="`/${article.post_categories[0].slug}/${article.slug}`"
             v-for="(article, i) in data.connectionArticles"
             :key="i"
             class="flex items-end gap-2 lg:block"
